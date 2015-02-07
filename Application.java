494910fd18344b821077a97d64762d49e87748c1
Captwalloper <captwalloper@ufl.edu>
2015-02-07 18:39:36 -0500

    Format fixed!!!

--- Application.java
+++ Application.java
@@ -1,13 +1,15 @@
+
 public class Application {
-  public static void main(String[] args) {
-    System.out.println("Hello!");
-    System.out.println("JR knows what is best!!!");
-    System.out.println("I'm Kun");
-    System.out.println("4 spaces or riot");
-	System.out.println("I'm Kun");
-	System.out.println("4 spaces or riot");
-        for (int i=0; i<100; i++) {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("JR knows what is best!!!");
+        System.out.println("I'm Kun");
+        System.out.println("4 spaces or riot");
+        System.out.println("I'm Kun");
+        System.out.println("4 spaces or riot");
+        for (int i = 0; i < 100; i++) {
             System.out.println("Jazz hands!");
         }
-  }
+    }
 }
