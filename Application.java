dcec7e40b2965e8a066196d3bb979924874bec6c
carlos.vizcaino@ufl.edu <dv112358>
2015-02-07 18:48:57 -0500

    Tiger

--- Application.java
+++ Application.java
@@ -1,6 +1,5 @@
 
 public class Application {
-
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("JR knows what is best!!!");
@@ -8,8 +7,10 @@ public class Application {
         System.out.println("4 spaces or riot");
         System.out.println("I'm Kun");
         System.out.println("4 spaces or riot");
+        System.out.println("Team Tiger: Is great!!!\n");
         for (int i = 0; i < 100; i++) {
             System.out.println("Jazz hands!");
+
         }
     }
 }
