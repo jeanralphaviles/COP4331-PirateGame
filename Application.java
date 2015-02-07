7339b9bad29a8f4d8d0e53e235bee8fbb11c69d4
kuncao0228 <kuncao0228@ufl.edu>
2015-02-07 18:26:56 -0500

    Kun Commit

--- Application.java
+++ Application.java
@@ -2,5 +2,6 @@ public class Application {
   public static void main(String[] args) {
     System.out.println("Hello!");
 	System.out.println("I'm Kun");
+	System.out.prtinln("4 spaces or riot");
   }
 }
