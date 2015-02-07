dfdffe76f18d5824bf72523d2c7850bddf427359
Captwalloper <captwalloper@ufl.edu>
2015-02-07 18:35:43 -0500

    Now with 100% more Jazz Hands!!!

--- Application.java
+++ Application.java
@@ -4,5 +4,10 @@ public class Application {
     System.out.println("JR knows what is best!!!");
     System.out.println("I'm Kun");
     System.out.println("4 spaces or riot");
+	System.out.println("I'm Kun");
+	System.out.println("4 spaces or riot");
+        for (int i=0; i<100; i++) {
+            System.out.println("Jazz hands!");
+        }
   }
 }
