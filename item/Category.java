b11d681f0ba1c72078cfff92ba4e1b332fe79fbe
jeanralphaviles <jeanralph.aviles@gmail.com>
2015-02-12 15:24:12 -0500

    Fixed Category Enum

--- item/Category.java
+++ item/Category.java
@@ -2,4 +2,18 @@ package item;
 
 public enum Category {
 	MAPTILE_SLOT(1);
-}
+
+	private int value;
+
+	private Category(int value) {
+		this.setValue(value);
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+};
