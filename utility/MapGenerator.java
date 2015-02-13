0af9897f2a6895ca132bbc38bead66064cc2c22d
James Risberg <james@verigo.io>
2015-02-13 17:34:33 -0500

    Convert ArrayList to 2D array for map generation

--- utility/MapGenerator.java
+++ utility/MapGenerator.java
@@ -15,43 +15,79 @@ import model.map.terrain.Water;
 
 public class MapGenerator {
 
-	public static Map generateMap(File file) {
+	public static Map generateMap(File file)
+	{
 		ArrayList< ArrayList<Maptile> > grid = new ArrayList< ArrayList<Maptile> >();
+
 		BufferedReader reader = null;
 		String line = "";
 		String csvSplit = ",";
+		int longest = 0;
 
 		try {
 			reader = new BufferedReader(new FileReader(file));
+
 			while ((line = reader.readLine()) != null) {
 				String[] row = line.split(csvSplit);
 				ArrayList<Maptile> rowTiles = new ArrayList<Maptile>();
+
+				if (row.length > longest) { longest = row.length; }
+
 				for (int j = 0; j < row.length; ++j) {
                     Maptile tile = new Maptile();
+
 					switch(row[j]) {
+
 					case "Grass":
 						tile.setTerrain(new Grass());
 						break;
+
 					case "Water":
 						tile.setTerrain(new Water());
 						break;
+
 					case "Mountain":
 						tile.setTerrain(new Mountain());
 						break;
+
 					default:
 						break;
 					}
+
 					rowTiles.add(tile);
 				}
+
 				grid.add(rowTiles);
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+
+		}
+
+		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		/* Convert 2d ArrayList to 2d Array */
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Maptile[][] customGrid = convertArrayListToArray(grid, longest);
+
 		return null;
 	}
 
+	private static Maptile[][] convertArrayListToArray(ArrayList<ArrayList<Maptile>> grid, int length)
+	{
+		Maptile[][] customGrid = new Maptile[length][grid.size()];
+
+		for (int i = 0; i < grid.size(); ++i) {
+			Maptile[] mapRow = (Maptile[]) (grid.get(i)).toArray();
+
+			for (int j = 0; j < length; ++j) {
+				if (mapRow[j] != null) { customGrid[j][i] = mapRow[j]; }
+				else { customGrid[j][i] = new Maptile(); }
+			}
+		}
+
+		return customGrid;
+	}
 }
