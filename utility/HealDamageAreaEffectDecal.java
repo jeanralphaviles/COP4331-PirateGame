d9d4baae0b938b059cced72966348bddc484a419
jeanralphaviles <jeanralph.aviles@gmail.com>
2015-02-13 13:04:19 -0500

    Decal for HealDamageAreaEffect

---
+++ utility/HealDamageAreaEffectDecal.java
@@ -0,0 +1,39 @@
+package utility;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public final class HealDamageAreaEffectDecal extends Decal {
+	private static final int spriteWidthpx = 32;
+	private static final int spriteHeightpx = 32;
+
+	public HealDamageAreaEffectDecal() {
+		File areaEffectFile = new File("./../Sprites/item/S_buff01.png");
+		try {
+			BufferedImage healDecal = ImageIO.read(areaEffectFile);
+			setImage(healDecal);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public HealDamageAreaEffectDecal(BufferedImage image) {
+		super(image);
+	}
+
+	public HealDamageAreaEffectDecal(File file) {
+		super(file);
+	}
+
+	public static int getSpriteheightpx() {
+		return spriteHeightpx;
+	}
+
+	public static int getSpritewidthpx() {
+		return spriteWidthpx;
+	}
+
+}
