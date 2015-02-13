a17ed6047b38f105a97c9c264f224e813120d2b1
jthakore <jthakore1@gmail.com>
2015-02-13 14:24:42 -0500

    testing commit and add so I don't make a mistake with screen

--- screen/Screen.java
+++ screen/Screen.java
@@ -1,11 +1,22 @@
 package screen;
 
 import model.Model;
+import controller.Controller;
 
 public abstract class Screen {
-
+	protected Controller controller;
+	protected Model model;
+	//protected ViewPort viewPort;
+	
 	public Screen(Model model) {
 		// TODO Auto-generated constructor stub
 	}
 
+	public abstract void init();
+	
+	public void updateView(GameObject gameObject){
+		//forwards message from model to viewPort
+	}
+	
+	
 }
