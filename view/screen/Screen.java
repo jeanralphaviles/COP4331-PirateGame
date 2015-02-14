764187ed487f409ff2159f81c0604e2498642afb
jthakore <jthakore1@gmail.com>
2015-02-14 16:12:00 -0500

    adding interfaces for all the viewPorts. need to code them with appropriate buttons and intentmap

--- view/screen/Screen.java
+++ view/screen/Screen.java
@@ -1,9 +1,13 @@
 package view.screen;
 
+import java.util.ArrayList;
+
+import utility.IntentComponentMap;
 import view.viewport.ViewPort;
 import model.GameObject;
 import model.Model;
 import controller.Controller;
+import controller.ScreenController;
 
 public abstract class Screen {
 	protected Controller controller;
@@ -12,13 +16,20 @@ public abstract class Screen {
 
 	public Screen(Model model) {
 		this.model = model;
-		init();
+		initialize();
 	}
 
+	public void initialize(){
+		init();
+		//From viewport, generate IntentComponentMaps
+		ArrayList<IntentComponentMap> icm = viewPort.generateIntentComponentMapping();
+		//Create controller from IntentComponentMaps
+		ScreenController s = new ScreenController(model, icm);
+	}
 	public abstract void init();
 
 	public final void updateView(GameObject gameObject) {
-		// TODO
+		//viewPort.updateView(gameObject);
 	}
 
 }
---
+++ view/viewport/AreaViewport.java
@@ -0,0 +1,5 @@
+package view.viewport;
+
+public class AreaViewport {
+
+}
